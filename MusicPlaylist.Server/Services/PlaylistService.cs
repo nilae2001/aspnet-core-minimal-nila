1095a02f6b6a2852c9ebc2f322d452b96ec04833
abdullokhonz <abdullokhon1206@gmail.com>
2026-04-10 08:10:25 +0500

    Refactor: migrate logic from Program.cs to Controllers and Services.

---
+++ MusicPlaylist.Server/Services/PlaylistService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicPlaylist.Server.Data;
+using MusicPlaylist.Server.DTOs;
+using MusicPlaylist.Server.Models;
+
+namespace MusicPlaylist.Server.Services
+{
+    public class PlaylistService
+    {
+        public readonly PlaylistDb _db;
+
+        public PlaylistService(PlaylistDb db)
+        {
+            this._db = db;
+        }
+
+        public async Task<IEnumerable<Playlist>> GetPlaylistsAsync(CancellationToken ct)
+        {
+            var playlists = await _db.Playlists.Include(p => p.Tracks).ToListAsync(ct);
+
+            return playlists;
+        }
+
+        public async Task<Playlist> GetPlaylistByIdAsync(int id, CancellationToken ct)
+        {
+            var playlist = await _db.Playlists.Include(p => p.Tracks).SingleOrDefaultAsync(p => p.Id == id, ct);
+
+            return playlist;
+        }
+
+        public async Task<Playlist> CreatePlaylistAsync(PlaylistDTO newPlaylist, CancellationToken ct)
+        {
+            var playlist = new Playlist { Title = newPlaylist.Title };
+
+            _db.Playlists.Add(playlist);
+
+            await _db.SaveChangesAsync(ct);
+
+            return playlist;
+        }
+
+        public async Task<bool> UpdatePlaylistAsync(int id, Playlist updatedPlaylist, CancellationToken ct)
+        {
+            var playlist = await _db.Playlists.FindAsync(new object[] { id }, ct);
+
+            if (playlist == null)
+                return false;
+
+            _db.Attach(playlist);
+
+            playlist.Title = updatedPlaylist.Title;
+
+            var result = await _db.SaveChangesAsync(ct);
+
+            return result > 0;
+        }
+
+        public async Task<bool> DeletePlaylistAsync(int id, CancellationToken ct)
+        {
+            var playlist = await _db.Playlists.FindAsync(new object[] { id }, ct);
+
+            if (playlist == null)
+                return false;
+
+            _db.Playlists.Remove(playlist);
+
+            var result = await _db.SaveChangesAsync(ct);
+
+            return result > 0;
+        }
+    }
+}
---
+++ MusicPlaylist.Server/Services/TrackService.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicPlaylist.Server.Data;
+using MusicPlaylist.Server.Models;
+
+namespace MusicPlaylist.Server.Services
+{
+    public class TrackService
+    {
+        public readonly PlaylistDb _db;
+
+        public TrackService(PlaylistDb db)
+        {
+            this._db = db;
+        }
+
+        public async Task<IEnumerable<Track>> GetTracksAsync(CancellationToken ct)
+        {
+            var tracks = await _db.Tracks.Include(t => t.Playlist).ToListAsync(ct);
+
+            return tracks;
+        }
+
+        public async Task<Track> GetTrackByIdAsync(int id, CancellationToken ct)
+        {
+            var track = await _db.Tracks.Include(t => t.Playlist).SingleOrDefaultAsync(t => t.Id == id, ct);
+
+            return track;
+        }
+
+        public async Task<Track> CreateTrackAsync(Track newTrack, CancellationToken ct)
+        {
+            _db.Tracks.Add(newTrack);
+
+            await _db.SaveChangesAsync(ct);
+
+            return newTrack;
+        }
+
+        public async Task<bool> UpdateTrackAsync(int id, Track updatedTrack, CancellationToken ct)
+        {
+            var track = await _db.Tracks.FindAsync(new object[] { id }, ct);
+
+            if (track == null)
+                return false;
+
+            _db.Attach(track);
+
+            track.Title = updatedTrack.Title;
+
+            track.Artist = updatedTrack.Artist;
+
+            var result = await _db.SaveChangesAsync(ct);
+
+            return result > 0;
+        }
+
+        public async Task<bool> DeleteTrackAsync(int id, CancellationToken ct)
+        {
+            var track = await _db.Tracks.FindAsync(new object[] { id }, ct);
+
+            if (track == null)
+                return false;
+
+            _db.Tracks.Remove(track);
+
+            var result = await _db.SaveChangesAsync(ct);
+
+            return result > 0;
+        }
+    }
+}
